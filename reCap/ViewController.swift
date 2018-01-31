7a1dff94b4895a68dd2b5d3e0b50bdfb48431175
Kaleb Cooper <kaleb.cooper@okstate.edu>
2018-01-31 15:36:11 -0600

    Initial Commit

---
+++ reCap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reCap
+//
+//  Created by Kaleb Cooper on 1/31/18.
+//  Copyright © 2018 Kaleb Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ reCapTests/reCapTests.swift
@@ -0,0 +1,36 @@
+//
+//  reCapTests.swift
+//  reCapTests
+//
+//  Created by Kaleb Cooper on 1/31/18.
+//  Copyright © 2018 Kaleb Cooper. All rights reserved.
+//
+
+import XCTest
+@testable import reCap
+
+class reCapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
