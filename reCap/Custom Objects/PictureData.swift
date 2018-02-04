b2443bb4baea23c1611d98a3ed04bbbf7861fd5a
Jackson Delametter <jacksondelametter@gmail.com>
2018-02-04 17:07:19 -0600

    Added read and write functionality for picture data. Also now able to read and write pictures to storage.

--- reCap/Custom Objects/PictureData.swift
+++ reCap/Custom Objects/PictureData.swift
@@ -13,23 +13,28 @@ class PictureData {
     // MARK: - Constants
     static let ORIENTATION_PORTRAIT = 0
     static let ORIENTATION_LANDSCAPE = 1
+    static let LONGITUDE_INDEX = 0
+    static let LATTITUDE_INDEX = 1
     
     // MARK: - Properties
     var name: String!
-    var gpsCoordinates: String!
+    var id: String!
+    var gpsCoordinates: [Double]!
     var orientation: Int!
     var owner: String!
     var time: String!
     var locationName: String!
     
     // MARK: - Initializers
-    public init(name: String!, gpsCoordinates: String, orientation: Int, owner: String, time: String, locationName: String) {
+    public init(name: String!, gpsCoordinates: [Double], orientation: Int, owner: String, time: String, locationName: String, id: String) {
         self.name = name
+        self.gpsCoordinates = []
         self.gpsCoordinates = gpsCoordinates
         self.orientation = orientation
         self.owner = owner
         self.time = time
         self.locationName = locationName
+        self.id = id
     }
     
 }
--- reCap/Database/FBDatabase.swift
+++ reCap/Database/FBDatabase.swift
@@ -18,6 +18,15 @@ class FBDatabase {
     private static let USER_PICTURES = "Pictures"
     private static let USER_EMAIL = "E-mail"
     private static let EMPTY_VALUE = "Empty"
+    private static let PICTURE_DATA_NAME = "Name"
+    private static let PICTURE_DATA_GPS = "Coordinates"
+    private static let PICTURE_DATA_ORIENTATION = "Orientation"
+    private static let PICTURE_DATA_OWNER = "Owner"
+    private static let PICTURE_DATA_TIME = "Time"
+    private static let PICTURE_DATA_LOCATION_NAME = "Location"
+    private static let PICTURE_DATA_NODE = "Picture Data"
+    private static let PICTURE_DATA_ID = "id"
+    private static let PICTURE_NODE = "Pictures"
     
     private static let LOGGED_IN_ID = "Logged in id"
     /*
@@ -115,6 +124,101 @@ class FBDatabase {
             else {
                 // No users in the database
                 completion(nil)
+            }
+        })
+    }
+    
+    // MARK: - Picture Data Methods
+    
+    /*
+     Adds/updates picture data to the database
+    */
+    
+    class func addUpdatePictureData(pictureData: PictureData, with_completion completion: @escaping (_ error: String?) -> ()) {
+        let jsonObject: [String : Any] = [PICTURE_DATA_NAME : pictureData.name, PICTURE_DATA_GPS : pictureData.gpsCoordinates, PICTURE_DATA_ORIENTATION : pictureData.orientation, PICTURE_DATA_OWNER : pictureData.owner, PICTURE_DATA_TIME : pictureData.time, PICTURE_DATA_LOCATION_NAME : pictureData.locationName]
+        let ref = Database.database().reference()
+        ref.child(PICTURE_DATA_NODE).child(pictureData.id).setValue(jsonObject, withCompletionBlock: {(error, ref) in
+            if let actualError = error {
+                // Has error
+                completion(actualError.localizedDescription)
+            }
+            else {
+                // No error
+                completion(nil)
+            }
+        })
+    }
+    
+    /*
+     Gets picture data from database
+    */
+    class func getPictureData(id: String, ref: DatabaseReference, with_completion completion: @escaping (_ pictureData: PictureData?) -> ()) {
+        ref.observe(.value, with: {(snapshot) in
+            let root = snapshot.value as! NSDictionary
+            if let pictureDataNode = root[PICTURE_DATA_NODE] as? NSDictionary {
+                // Database has picture data in it
+                let pictureDataData = pictureDataNode[id] as! NSDictionary
+                let name = pictureDataData[PICTURE_DATA_NAME] as! String
+                let coordinates = pictureDataData[PICTURE_DATA_GPS] as! [Double]
+                let orientation = pictureDataData[PICTURE_DATA_ORIENTATION] as! Int
+                let owner = pictureDataData[PICTURE_DATA_OWNER] as! String
+                let time = pictureDataData[PICTURE_DATA_TIME] as! String
+                let locationName = pictureDataData[PICTURE_DATA_LOCATION_NAME] as! String
+                let id = pictureDataData[PICTURE_DATA_ID] as! String
+                let pictureData = PictureData(name: name, gpsCoordinates: coordinates, orientation: orientation, owner: owner, time: time, locationName: locationName, id: id)
+                completion(pictureData)
+            }
+            else {
+                // Database does not have picture data in it
+                completion(nil)
+            }
+        })
+    }
+    
+    // MARK: - Storage Methods
+    
+    /*
+     Puts an image into the database
+    */
+    class func addPicture(image: UIImage, pictureData: PictureData, with_completion completion: @escaping (_ error: String?) -> ()) {
+        let storageRef = Storage.storage().reference(forURL: "gs://recap-78bda.appspot.com").child(PICTURE_NODE).child(pictureData.id)
+        
+        if let imageData = UIImageJPEGRepresentation(image, 0.2) {
+            storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                if error != nil {
+                    // Image was not written to database
+                    completion(error?.localizedDescription)
+                }
+                else {
+                    // Image was written to database
+                    completion(nil)
+                }
+            })
+        }
+    }
+    
+    /*
+     Get picture from database
+    */
+    class func getPicture(pictureData: PictureData, with_progress progress: @escaping (_ progress: Int64, _ total: Int64) -> (), with_completion completion: @escaping (_ image: UIImage?) -> ()) {
+        let storageRef = Storage.storage().reference(forURL: "gs://recap-78bda.appspot.com").child(PICTURE_NODE).child(pictureData.id)
+        
+        let downloadTask = storageRef.getData(maxSize: 1 * 1024 * 1024) { (data, error) -> Void in
+            if data != nil {
+                // Got picture in database
+                let pic = UIImage(data: data!)
+                completion(pic)
+            }
+            else {
+                // Could not get image in database
+                completion(nil)
+            }
+        }
+        downloadTask.observe(.progress, handler: {(snapshot) in
+            let currentProgress = snapshot.progress?.completedUnitCount
+            let totalCount = snapshot.progress?.totalUnitCount
+            if totalCount != 0 {
+                progress(currentProgress!, totalCount!)
             }
         })
     }
